9dd815f5e084b35897f4fae115fea4fde292c49d
Leo <131379171+NM001007@users.noreply.github.com>
2023-06-22 19:30:11 +0330

    Add files via upload

---
+++ Algorithm.py
@@ -0,0 +1,118 @@
+import networkx as nx
+import random
+
+def fatehi_algorithm(network, iterations, source_node, target_node, trust_values):
+    ## reward and penalty parameters
+    a = 0.009
+    b = 0.00009
+    R = 5
+
+    total_neighbors = set()
+    for node in network:
+        total_neighbors = total_neighbors.union(nx.neighbors(network, node))
+
+    ## Phase 1: Constructing DLA Isomorphic to Trust Network
+    Automatas = dict()
+    action_set = dict()
+    action_prob = dict()
+    reward = dict()
+    W_i = dict()
+    for node in network.nodes:
+        Automatas[node] = 0
+        action_set[node] = list(nx.neighbors(network, node))
+        # temp = dict()
+        # for item in action_set[node]:
+        #     temp[item] = 1/len(action_set[node])
+        # action_prob[node] = temp
+        action_prob[node] = 1/len(total_neighbors)
+        W_i[node] = 0
+        reward[node] = 0
+
+    k_n = sum(action_prob.values())
+
+    ## Phase 2: Learning Reliability of Direct Trust Values
+    n_r = 0
+    k = -1
+    path = []
+    while k < iterations:
+        k += 1
+        path = []
+        v_k = source_node
+        # A_k = Automatas[v_k]
+        path.append(source_node)
+        Automatas[v_k] = 1
+
+        available_actions = list(action_set[v_k])
+        while (not len(available_actions)) or (v_k in action_set[target_node]):
+            available_actions = list(action_set[v_k])
+            intersection = set(available_actions).intersection(set(path))
+            if len(intersection):
+                for item in intersection:
+                    set(available_actions).remove(item)
+
+            if len(available_actions):
+                if v_k == source_node:
+                    index = random.randint(0, len(available_actions) - 1)
+                    v_k = available_actions[index]
+                    path.append(v_k)
+                    Automatas[v_k]=1
+                else:
+                    # values = list(action_prob[v_k].values())
+                    # keys = list(action_prob[v_k].keys())
+                    # max_p = max(values)
+                    # for i in range(len(keys)):
+                    #     if (values[i] >= max_p) and (not i in path):
+                    #         v_k=i
+                    #         print(v_k, path)
+                    # path.append(v_k)
+                    # Automatas[v_k] = 1
+                    temp = dict()
+                    keys = list(action_set[v_k])
+                    for i in keys:
+                        if not i in path:
+                            temp[i] = action_prob[i]
+                    max_value = max(temp.values())
+                    v_k = list(temp.keys())[list(temp.values()).index(max_value)]
+                    path.append(v_k)
+                    Automatas[v_k] = 1
+
+            ST_sk = 1
+            for i in range(0, len(path)-1):
+                key = (path[i],path[i+1])
+                if key in trust_values.keys():
+                    if trust_values[key] <= ST_sk:
+                        ST_sk = trust_values[key] #min strategy
+                    # ST_sk *= trust_values[key] #multi strategy
+
+            if (v_k in action_set[target_node]) and ST_sk >= W_i[v_k]: ##Favorable
+                for n in path:
+                    reward[n] = reward[n] + a
+                    action_prob[n] = action_prob[n] + a*(1-action_prob[n])
+                    if n!=v_k: #j!=i
+                        action_prob[n] = (1-a) * action_prob[n]
+                W_i[v_k] = ST_sk
+                n_r += 1
+            else:
+                for n in path:   #Punishment
+                    reward[n] = reward[n] - b
+                    action_prob[n] = (1-b) * action_prob[n]
+                    if n!=v_k: #j!=i
+                        action_prob[n] = (b/(reward[n]-1)) + (1-b) * action_prob[n]
+                n_r = 0
+
+            k_n = sum(action_prob.values())
+            for p in action_prob:
+                action_prob[p] = action_prob[p]/k_n
+
+    ## Phase3.Aggregating Direct Trust Values
+    part_1 = 0
+    part_2 = 0.000000000001
+    for item in W_i:
+        if W_i[item]:
+            key = (item, target_node)
+            part_2 += W_i[item]
+            if key in trust_values.keys():
+                part_1 += (W_i[item]*trust_values[key])
+
+    final_trust_value = part_1/part_2
+    return final_trust_value
---
+++ main.py
@@ -0,0 +1,73 @@
+import re
+import time
+import networkx as nx
+from nina_fatehi.Algorithm import *
+
+def read_advogato():
+    file_main = open("../DataSet/advogato/advogato_full_reformed.txt", 'r')
+    read = file_main.readlines()
+    file_main.close()
+
+    advogato = dict()
+    for item in read:
+        line = re.split('\n',item)[0]
+        line = re.split(' ', line)
+        if len(line)>1 and line[0] != line[1]:
+            advogato[(line[0],line[1])] = float(line[2])
+
+    return advogato
+
+def read_dataset():
+    file = open("./Data/soc-sign-bitcoinalpha.csv", 'r')
+    read = file.readlines()
+    file.close()
+    trust_values = dict()
+    for line in read[1:]:
+        items = re.split(',', line)
+        trust_values[(items[0],items[1])] = (int(items[2])+10)/20
+    return trust_values
+
+if __name__ == '__main__':
+    time_base = time.time()
+    # advogato = read_advogato()
+    # advogato_duplicate = advogato.copy()
+    # network = nx.Graph(list(advogato.keys()))
+
+    trust_values = read_dataset()
+    list_of_keys = list(trust_values.keys())
+    trust_values_duplicate = trust_values.copy()
+    network = nx.Graph(list(trust_values.keys()))
+
+    number_iteration = 700
+    # source = "itamar"
+    # target = "carmstro"
+    # fatehi_algorithm(network,number_iteration, source, target, advogato)
+
+    trust = dict()
+
+    # for item in list(advogato.keys())[13000:14000]:    ##all paths
+    for item in list(trust_values.keys())[10000:12000]:    ##all paths
+        # advogato = advogato_duplicate.copy()
+        advogato = trust_values_duplicate.copy()
+        network = nx.Graph(list(advogato.keys()))
+
+        source = item[0]
+        target = item[1]
+
+        print(list(advogato.keys()).index(item),"---- Source:", source, "and Target:", target,"----")
+
+        if source != target:
+            t_check = advogato[(source,target)]
+            advogato.pop((source,target))
+
+            trust[item] = fatehi_algorithm(network,number_iteration, source, target, advogato)
+            print(trust[item], t_check)
+
+
+        file = open("results_fatehi_temp.txt", 'w')
+        for key in trust.keys():
+            string = str(key[0])+ " "+ str(key[1]) + " "+ str(trust[key])+ " \n"
+            file.writelines(string)
+        file.close()
+
+    print("The whole process, from top to bottom, took", (time.time()-time_base)/3600, "hours")
